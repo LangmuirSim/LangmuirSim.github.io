5234f744d287528d351a0d6ad6e6f577ecdbff0b
Adam Gagorik <adam.gagorik@gmail.com>
2021-01-06 09:53:26 -0500

    Add build script

---
+++ bin/build.sh
@@ -0,0 +1,8 @@
+cd .. || exit
+python ./bin/clean.py
+
+cd ./doc || exit
+./source/scripts/generate.sh
+sphinx-build -b html -d build/doctrees source build/html || exit
+
+# mv -fv ./build/html/* ./
---
+++ bin/clean.py
@@ -0,0 +1,38 @@
+import shutil
+import os
+
+work = os.getcwd()
+
+exclude_d = [
+    './.git',
+    './bin',
+    './doc',
+    '.',
+]
+exclude_d = list(map(lambda x: os.path.abspath(x), exclude_d))
+
+exclude_f = [
+    './index.html',
+    './Makefile',
+    './.nojekyll',
+]
+exclude_f = list(map(lambda x: os.path.abspath(x), exclude_f))
+
+found_d = []
+found_f = []
+
+root, found_d, found_f = next(os.walk(work))
+found_d = map(lambda x: os.path.join(work, x), found_d)
+found_f = map(lambda x: os.path.join(work, x), found_f)
+
+found_d = [d for d in found_d if not d in exclude_d]
+found_f = [f for f in found_f if not f in exclude_f]
+
+for d in found_d:
+    print('rm -rf %s' % d)
+    shutil.rmtree(d)
+
+for f in found_f:
+    print('rm -f %s' % f)
+    os.remove(f)
+
